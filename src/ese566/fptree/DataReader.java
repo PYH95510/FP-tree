55bce8b6b68ebee3ea99432e610c05506965a6b8
Suyoung Choi <kes5219@gmail.com>
2022-10-03 21:02:53 +0000

    Read list of transactions

--- src/ese566/fptree/DataReader.java
+++ src/ese566/fptree/DataReader.java
@@ -2,11 +2,15 @@ package ese566.fptree;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class DataReader {
-	static HashMap<Integer, Integer> dataNums = new HashMap<Integer, Integer>();
+	// <Attribute ID, Count>
+	static HashMap<Integer, Integer> attribCount = new HashMap<Integer, Integer>();
+	// <Transaction ID, <Attribute ID>>
+	static HashMap<Integer, ArrayList<Integer>> transactions = new HashMap<>();
 
 	public static void readNetDFile(String fileName) {
 		BufferedReader reader = null;
@@ -14,19 +18,33 @@ public class DataReader {
 		try {
 			reader = new BufferedReader(new FileReader(fileName));
 			String line;
-			String comma = ",";
-
+			String tokens = ",";
+			int curTransactionID = -1;
+			
 			reader.readLine();// ignore 0
-
 			line = reader.readLine();
 			while (line != null) {
-				if (line.contains(" v ")) { // if line has v
-					StringTokenizer st = new StringTokenizer(line, comma);
-					int data = Integer.parseInt(st.nextToken());
-
-					dataNums.put(data, dataNums.getOrDefault(data, 1) + 1);
+				StringTokenizer st = new StringTokenizer(line, tokens);
+				String typeStr = st.nextToken();
+				if (typeStr.equals("C")) { // If line is a transaction ("C")
+					String transactionIDStr = st.nextToken();
+					// Remove enclosing quotes
+					transactionIDStr = transactionIDStr.replace("\"", "");
+					curTransactionID = Integer.parseInt(transactionIDStr);
 				}
-				
+				else if (typeStr.equals("V")) { // If line is an item for a transaction ("V")
+					if (curTransactionID < 0) {
+						// This shouldn't happen. All "V" should always come after "C" and therefore
+						// curTransactionID should always be initialized
+						throw new RuntimeException("Data is incorrectly formatted");
+					}
+					// Attribute ID
+					int attribID = Integer.parseInt(st.nextToken());
+					attribCount.put(attribID, attribCount.getOrDefault(attribID, 0) + 1);
+
+					var itemset = transactions.getOrDefault(curTransactionID, new ArrayList<Integer>());
+					itemset.add(attribID);
+				}
 				line = reader.readLine();
 			}
 		} catch (IOException e) {
