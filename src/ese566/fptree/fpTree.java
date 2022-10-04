af157940433e1f246b4ecf05798c1346a40a3fd9
Yohan Park <pyh95510@gmail.com>
2022-10-03 21:56:36 -0400

    fptree

---
+++ src/ese566/fptree/fpTree.java
@@ -0,0 +1,22 @@
+package ese566.fptree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import ese566.fptree.DataReader;
+
+public class fpTree {
+
+    fpTree() {
+        HashMap<Integer, Integer> child = new HashMap<>();
+
+    }
+
+    public void generateTree(HashMap<Integer, ArrayList<Integer>> itemset) {
+
+        for (HashMap.Entry<Integer, ArrayList<Integer>> ent : itemset.entrySet()) {
+
+        } // for every entry in the itemsets
+
+    }
+
+}
