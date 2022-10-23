b4589a477872320e22aa3228dd7677c6c1fda0ff
Yohan Park <pyh95510@gmail.com>
2022-10-23 04:05:40 -0400

    package duplicate change

--- src/ese566/fptree/FPTree.java
+++ src/ese566/fptree/FPTree.java
@@ -9,35 +9,28 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 
-import ese566.fptree.DataReader;
-
-public class FPTree 
-{
+public class FPTree {
     private TreeNode root = new TreeNode(0);
     private DataSet m_dataset;
     private HashMap<Integer, TreeNode> m_table = new HashMap<Integer, TreeNode>();
 
-    private FPTree() 
-    {
+    private FPTree() {
 
     }
 
     /**
      * @param item
      */
-    static public FPTree create(DataSet input) 
-    {
+    static public FPTree create(DataSet input) {
         FPTree fpTree = new FPTree();
         fpTree.m_dataset = input;
 
-        for (int key : input.getKeytransaction()) 
-        { 
+        for (int key : input.getKeytransaction()) {
             // second iteration
             ArrayList<Integer> items = new ArrayList<Integer>(input.m_transactions.get(key)); // getting each value for
-            TreeNode curNode = fpTree.root; 
+            TreeNode curNode = fpTree.root;
             // each key
-            for (int j = 0; j < items.size(); j++) 
-            {
+            for (int j = 0; j < items.size(); j++) {
                 int itemId = items.get(j);
                 TreeNode nextNode = curNode.addChild(itemId, input, fpTree.m_table);
                 curNode = nextNode;
@@ -47,26 +40,20 @@ public class FPTree
         return fpTree;
     }
 
-    public void prune(int minimumSupport) 
-    {
+    public void prune(int minimumSupport) {
         root.pruneChildren(minimumSupport);
     }
 
-    public void extractPattern(int minSupport)
-    {
-        for (var iter = m_dataset.iterateFromMinSupport(minSupport); iter.hasNext();)
-        {
+    public void extractPattern(int minSupport) {
+        for (var iter = m_dataset.iterateFromMinSupport(minSupport); iter.hasNext();) {
             HashMap<Integer, Integer> condTable = new HashMap<>();
             Integer itemId = iter.next();
             TreeNode curNode = m_table.getOrDefault(itemId, null);
-            while (curNode != null)
-            {
+            while (curNode != null) {
                 condTable.put(itemId, condTable.getOrDefault(itemId, 0) + 1);
                 TreeNode curParentNode = curNode.getParent();
-                while (true)
-                {
-                    if (curParentNode.isRoot())
-                    {
+                while (true) {
+                    if (curParentNode.isRoot()) {
                         break;
                     }
                     condTable.put(curParentNode.getName(), condTable.getOrDefault(curParentNode.getName(), 0) + 1);
@@ -76,31 +63,27 @@ public class FPTree
                 curNode = curNode.getNext();
             }
 
-            // Re-iterate the tree to gather the entries that have higher support than the minimum
-            HashSet<ArrayList<Integer>> entries = new HashSet<>(); 
+            // Re-iterate the tree to gather the entries that have higher support than the
+            // minimum
+            HashSet<ArrayList<Integer>> entries = new HashSet<>();
             curNode = m_table.getOrDefault(itemId, null);
-            while (curNode != null)
-            {
+            while (curNode != null) {
                 ArrayList<Integer> entry = new ArrayList<>();
                 entry.add(curNode.getName());
 
                 TreeNode curParentNode = curNode.getParent();
-                while (true)
-                {
-                    if (curParentNode.isRoot())
-                    {
+                while (true) {
+                    if (curParentNode.isRoot()) {
                         break;
                     }
-                    
-                    if (condTable.getOrDefault(curParentNode.getName(), 0) >= minSupport)
-                    {
+
+                    if (condTable.getOrDefault(curParentNode.getName(), 0) >= minSupport) {
                         entry.add(curParentNode.getName());
                     }
                     curParentNode = curParentNode.getParent();
                 }
                 // Only add if there are more than 1 item in the frequent set
-                if (entry.size() > 1)
-                {
+                if (entry.size() > 1) {
                     entries.add(entry);
                 }
 
@@ -109,20 +92,15 @@ public class FPTree
 
             // Print the result
             StringBuilder sb = new StringBuilder();
-            for (var entry : entries)
-            {
-                if (entry.size() <= 1)
-                {
+            for (var entry : entries) {
+                if (entry.size() <= 1) {
                     continue;
                 }
                 sb.append("[");
-                for (int i = 0; i < entry.size(); ++i)
-                {
-                    if (i != entry.size() - 1)
-                    {
+                for (int i = 0; i < entry.size(); ++i) {
+                    if (i != entry.size() - 1) {
                         sb.append(entry.get(i) + ", ");
-                    } else
-                    {
+                    } else {
                         sb.append(entry.get(i));
                     }
                 }
@@ -132,8 +110,7 @@ public class FPTree
         }
     }
 
-    public void printName(StringBuilder sb) 
-    {
+    public void printName(StringBuilder sb) {
         root.printName(sb, 0);
     }
 }
