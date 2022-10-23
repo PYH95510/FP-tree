401d8fa8af600303931853de030a762bc5bb6723
Yohan Park <pyh95510@gmail.com>
2022-10-23 03:30:30 -0400

    restore

--- src/ese566/fptree/DataSet.java
+++ src/ese566/fptree/DataSet.java
@@ -8,29 +8,23 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class DataSet 
-{
+public class DataSet {
     public HashMap<Integer, Integer> m_supports;
     public HashMap<Integer, ArrayList<Integer>> m_transactions;
 
     public DataSet(
             HashMap<Integer, Integer> supports,
-            HashMap<Integer, ArrayList<Integer>> transactions) 
-    {
+            HashMap<Integer, ArrayList<Integer>> transactions) {
         m_supports = supports;
         m_transactions = transactions;
     }
 
-    public void sortTransactions() 
-    {
-        for (var kvPair : m_transactions.entrySet()) 
-        {
+    public void sortTransactions() {
+        for (var kvPair : m_transactions.entrySet()) {
             var itemSet = kvPair.getValue();
-            Collections.sort(itemSet, new Comparator<Integer>() 
-            {
+            Collections.sort(itemSet, new Comparator<Integer>() {
                 @Override
-                public int compare(Integer arg0, Integer arg1) 
-                {
+                public int compare(Integer arg0, Integer arg1) {
                     // If the attribute ID isn't in the list, then use -1 as the count. This
                     // shouldn't happen
                     // but is included for robustness
@@ -38,8 +32,7 @@ public class DataSet
                     int arg1Count = m_supports.getOrDefault(arg1, -1);
 
                     // Tie-breaker. If both counts are the same, then compare them by the IDs
-                    if (arg0Count == arg1Count) 
-                    {
+                    if (arg0Count == arg1Count) {
                         return arg1 - arg0;
                     }
                     // Note that the return value doesn't have to be {-1, 0, 1}. Returning any
@@ -54,19 +47,15 @@ public class DataSet
         }
     }
 
-    public Iterator<Integer> iterateFromMinSupport(int minSupport)
-    {
+    public Iterator<Integer> iterateFromMinSupport(int minSupport) {
         // Create a sorted set that sorts based on the support of the item ID
-        var prunedItemIds = new TreeSet<Integer>(new Comparator<Integer>()
-        {
+        var prunedItemIds = new TreeSet<Integer>(new Comparator<Integer>() {
             @Override
-            public int compare(Integer arg0, Integer arg1)
-            {
+            public int compare(Integer arg0, Integer arg1) {
                 int arg0Support = m_supports.get(arg0);
                 int arg1Support = m_supports.get(arg1);
                 // Tie-breaker
-                if (arg0Support == arg1Support)
-                {
+                if (arg0Support == arg1Support) {
                     return arg0 - arg1;
                 }
                 // Note that we need to sort in ascending order
@@ -74,48 +63,40 @@ public class DataSet
             }
         });
         // Remove items that does not meet the minimum support requirement
-        for (Integer itemId : m_supports.keySet())
-        {
-            if (m_supports.getOrDefault(itemId, 0) >= minSupport)
-            {
+        for (Integer itemId : m_supports.keySet()) {
+            if (m_supports.getOrDefault(itemId, 0) >= minSupport) {
                 prunedItemIds.add(itemId);
             }
         }
         return prunedItemIds.iterator();
     }
 
-    public int gettransaction() 
-    {
+    public int gettransaction() {
         return this.m_transactions.size();
 
     }
 
-    public int getSupport(int attribId) 
-    {
+    public int getSupport(int attribId) {
         return this.m_supports.getOrDefault(attribId, 0);
     }
 
-    public Set<Integer> getKeytransaction() 
-    {
+    public Set<Integer> getKeytransaction() {
         sortTransactions();
         return this.m_transactions.keySet();
     }
 
     @Override
-    public String toString() 
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         var transIter = m_transactions.entrySet().iterator();
-        while (transIter.hasNext()) 
-        {
+        while (transIter.hasNext()) {
             var kvPair = transIter.next();
             int transactionID = kvPair.getKey();
             var items = kvPair.getValue();
             String line = String.format("Transaction ID %s: ", transactionID);
 
             var itemsetIter = items.iterator();
-            while (itemsetIter.hasNext()) 
-            {
+            while (itemsetIter.hasNext()) {
                 int itemID = itemsetIter.next();
                 line += itemID + "(" + m_supports.getOrDefault(itemID, -1) + ") ";
             }
