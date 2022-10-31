49d9e2c2ebb937d7d9251911c45d3909818c64ab
Yohan Park <pyh95510@gmail.com>
2022-10-30 23:03:53 -0400

    minor change

--- src/ese566/fptree/App.java
+++ src/ese566/fptree/App.java
@@ -1,22 +1,21 @@
 package ese566.fptree;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import ese566.fptree.DataReader;
 import ese566.fptree.FPTree;
 
-public class App 
-{
-    public static void main(String[] args)
-    {
-        DataSet dataSet = DataReader.readNetDFile("/devel/FP-tree/data/anonymous-msweb.data");
+public class App {
+    public static void main(String[] args) {
+        DataSet dataSet = DataReader.readNetDFile("/Users/yohanpark/git/FPtree/data/anonymous-msweb.data");
 
         FPTree fpTree = FPTree.create(dataSet);
-        
+
         StringBuilder sb = new StringBuilder();
         fpTree.printName(sb);
         System.out.println(sb.toString());
 
         fpTree.extractPattern(220);
-    }    
+    }
 }
